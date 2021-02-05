32d5d65729fdd7046c68fa79e4ec0fec400375c8
smack322 <smcanally11@yahoo.com>
2021-02-05 18:11:43 -0500

    Delete routes directory

--- routes/authroute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router()
-// const registerController = require('../controllers/auth.controller');
-
-const {
-    registerController
-} = require('../controllers/auth.controller');
-router.post('/register', registerController)
-module.exports = router;
